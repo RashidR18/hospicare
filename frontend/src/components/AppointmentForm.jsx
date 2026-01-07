4f5facc037978863eb7b664ea392b4caa84ba141
Rashid <rashidali18november@gmail.com>
2026-01-08 00:27:34 +0530

    Update each and every backend api url in frontend

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -34,7 +34,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/doctors",
+        `${import.meta.env.VITE_API_URL}/api/v1/user/doctors`,
         { withCredentials: true }
       );
       setDoctors(data.doctors);
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          `${import.meta.env.VITE_API_URL}/api/v1/user/patient/register`,
           { firstName, lastName, email, phone, adhar, dob, gender, password ,role:"Patient"},
           {
             withCredentials: true,
